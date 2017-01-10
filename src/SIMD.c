d54bf629192fa87d01fe87cffbc3953ea3d6c3de
ReneBrals <brals.rene@gmail.com>
2017-01-10 23:10:42 +0100

    Added fallback from AVX2 to SSE2 or non-SIMD

--- src/SIMD.c
+++ src/SIMD.c
@@ -1,7 +1,9 @@
 #include "SIMD.h"
 #include <stdio.h>
 #include <string.h>
-#include <omp.h>
+
+#ifdef __AVX2__
+//Processing 32 bytes at a time will require AVX2 support (Intel Haswell and newer)
 
 // Stores the minimum of a[i] and b[i] into c[i]
 void simdMin(unsigned char* c, unsigned char* a, unsigned char* b, size_t x){
@@ -62,3 +64,116 @@ void simdSubInPlace(unsigned char* a, unsigned char* b, size_t x){
         a[i] = a[i] > b[i] ? a[i]-b[i] : 0;
     }
 }
+
+#elif __SSE2__
+//Processing 16 bytes at a time will require SSE2 support (Intel NetBurst and newer)
+
+// Stores the minimum of a[i] and b[i] into c[i]
+void simdMin(unsigned char* c, unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+    for(i=0; i < x/16; i++){
+        min16x8(&(c[i*16]),&(a[i*16]),&(b[i*16]));
+    }
+
+    for(i = x - (x % 16); i<x; i++){
+            c[i] = b[i] < a[i] ? b[i] : a[i];
+    }
+}
+
+// Stores the minimum of a[i] and b[i] into a[i]
+void simdMinInPlace(unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+    for(i=0; i < x/16; i++){
+        min16x8_ip(&(a[i*16]),&(b[i*16]));
+    }
+
+    for(i = x - (x % 16); i<x; i++){
+        if(b[i] < a[i] ) a[i] = b[i];
+    }
+}
+
+// Stores the maximum of a[i] and b[i] into c[i]
+void simdMax(unsigned char* c, unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+    for(i=0; i < x/16; i++){
+        max16x8(&(c[i*16]),&(a[i*16]),&(b[i*16]));
+    }
+
+    for(i = x - (x % 16); i<x; i++){
+            c[i] = b[i] > a[i] ? b[i] : a[i];
+    }
+}
+
+// Stores the maximum of a[i] and b[i] into a[i]
+void simdMaxInPlace(unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+    for(i=0; i < x/16; i++){
+        max16x8_ip(&(a[i*16]),&(b[i*16]));
+    }
+
+    for(i = x - (x % 16); i<x; i++){
+        if(b[i] > a[i] ) a[i] = b[i];
+    }
+}
+
+// Subtracts b[i] from a[i], storing the result in a[i]
+void simdSubInPlace(unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+    for(i=0; i < x/16; i++){
+        sub16x8_ip(&(a[i*16]),&(b[i*16]));
+    }
+
+    for(i = x - (x % 16); i<x; i++){
+        a[i] = a[i] > b[i] ? a[i]-b[i] : 0;
+    }
+}
+
+#else
+//If SSE2 is not available, fall back to the boring way of doing things...
+
+// Stores the minimum of a[i] and b[i] into c[i]
+void simdMin(unsigned char* c, unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+
+    for(i = 0; i < x; i++){
+        c[i] = b[i] < a[i] ? b[i] : a[i];
+    }
+}
+
+// Stores the minimum of a[i] and b[i] into a[i]
+void simdMinInPlace(unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+
+    for(i = 0; i < x; i++){
+        if(b[i] < a[i] ) a[i] = b[i];
+    }
+}
+
+// Stores the maximum of a[i] and b[i] into c[i]
+void simdMax(unsigned char* c, unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+
+    for(i = 0; i < x; i++){
+        c[i] = b[i] > a[i] ? b[i] : a[i];
+    }
+}
+
+// Stores the maximum of a[i] and b[i] into a[i]
+void simdMaxInPlace(unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+
+    for(i = 0; i < x; i++){
+        if(b[i] > a[i] ) a[i] = b[i];
+    }
+}
+
+// Subtracts b[i] from a[i], storing the result in a[i]
+void simdSubInPlace(unsigned char* a, unsigned char* b, size_t x){
+    size_t i;
+
+    for(i = 0; i < x; i++){
+        a[i] = a[i] > b[i] ? a[i]-b[i] : 0;
+    }
+}
+
+#endif
--- src/SIMD.h
+++ src/SIMD.h
@@ -5,28 +5,13 @@
 #include <immintrin.h>
 #include <smmintrin.h>
 
-#define _mm_cmpge_epu8(a, b) \
-        _mm_cmpeq_epi8(_mm_max_epu8(a, b), a)
-
-#define _mm_cmple_epu8(a, b) _mm_cmpge_epu8(b, a)
+#ifdef __AVX2__
+//Processing 32 bytes at a time will require AVX2 support (Intel Haswell and newer)
 
 #define _mm256_cmpge_epu8(a, b) \
         _mm256_cmpeq_epi8(_mm256_max_epu8(a, b), a)
 
 #define _mm256_cmple_epu8(a, b) _mm256_cmpge_epu8(b, a)
-
-
-static inline void min16x8(unsigned char* c, unsigned char* a, unsigned char* b){
-    __m128i x = _mm_loadu_si128((const __m128i*)a);
-    __m128i y = _mm_loadu_si128((const __m128i*)b);
-    _mm_storeu_si128((__m128i*)c,_mm_min_epu8(x, y));
-}
-
-static inline void min16x8_ip(unsigned char* a, unsigned char* b){
-    __m128i x = _mm_loadu_si128((const __m128i*)a);
-    __m128i y = _mm_loadu_si128((const __m128i*)b);
-    _mm_storeu_si128((__m128i*)a,_mm_min_epu8(x, y));
-}
 
 static inline void min32x8(unsigned char* c, unsigned char* a, unsigned char* b){
     __m256i x = _mm256_loadu_si256((const __m256i*)a);
@@ -38,18 +23,6 @@ static inline void min32x8_ip(unsigned char* a, unsigned char* b){
     __m256i x = _mm256_loadu_si256((const __m256i*)a);
     __m256i y = _mm256_loadu_si256((const __m256i*)b);
     _mm256_storeu_si256((__m256i*)a,_mm256_min_epu8(x, y));
-}
-
-static inline void max16x8(unsigned char* c, unsigned char* a, unsigned char* b){
-    __m128i x = _mm_loadu_si128((const __m128i*)a);
-    __m128i y = _mm_loadu_si128((const __m128i*)b);
-    _mm_storeu_si128((__m128i*)c,_mm_max_epu8(x, y));
-}
-
-static inline void max16x8_ip(unsigned char* a, unsigned char* b){
-    __m128i x = _mm_loadu_si128((const __m128i*)a);
-    __m128i y = _mm_loadu_si128((const __m128i*)b);
-    _mm_storeu_si128((__m128i*)a,_mm_max_epu8(x, y));
 }
 
 static inline void max32x8(unsigned char* c, unsigned char* a, unsigned char* b){
@@ -69,6 +42,47 @@ static inline void sub32x8_ip(unsigned char* a, unsigned char* b){
     __m256i y = _mm256_loadu_si256((const __m256i*)b);
     _mm256_storeu_si256((__m256i*)a,_mm256_subs_epu8(x, y));
 }
+
+#elif __SSE2__
+//Processing 16 bytes at a time will require SSE2 support (Intel NetBurst and newer)
+
+#define _mm_cmpge_epu8(a, b) \
+        _mm_cmpeq_epi8(_mm_max_epu8(a, b), a)
+
+#define _mm_cmple_epu8(a, b) _mm_cmpge_epu8(b, a)
+
+static inline void min16x8(unsigned char* c, unsigned char* a, unsigned char* b){
+    __m128i x = _mm_loadu_si128((const __m128i*)a);
+    __m128i y = _mm_loadu_si128((const __m128i*)b);
+    _mm_storeu_si128((__m128i*)c,_mm_min_epu8(x, y));
+}
+
+static inline void min16x8_ip(unsigned char* a, unsigned char* b){
+    __m128i x = _mm_loadu_si128((const __m128i*)a);
+    __m128i y = _mm_loadu_si128((const __m128i*)b);
+    _mm_storeu_si128((__m128i*)a,_mm_min_epu8(x, y));
+}
+
+
+static inline void max16x8(unsigned char* c, unsigned char* a, unsigned char* b){
+    __m128i x = _mm_loadu_si128((const __m128i*)a);
+    __m128i y = _mm_loadu_si128((const __m128i*)b);
+    _mm_storeu_si128((__m128i*)c,_mm_max_epu8(x, y));
+}
+
+static inline void max16x8_ip(unsigned char* a, unsigned char* b){
+    __m128i x = _mm_loadu_si128((const __m128i*)a);
+    __m128i y = _mm_loadu_si128((const __m128i*)b);
+    _mm_storeu_si128((__m128i*)a,_mm_max_epu8(x, y));
+}
+
+static inline void sub16x8_ip(unsigned char* a, unsigned char* b){
+    __m128i x = _mm_loadu_si128((const __m128i*)a);
+    __m128i y = _mm_loadu_si128((const __m128i*)b);
+    _mm_storeu_si128((__m128i*)a,_mm_subs_epu8(x, y));
+}
+
+#endif
 
 void simdMin(unsigned char* c, unsigned char* a, unsigned char* b, size_t x);
 void simdMinInPlace(unsigned char* a, unsigned char* b, size_t x);
